0ebc1ff930f9ed15f2a6267d3502a77bd5f941db
Sepidehri <sepidriahi@gmail.com>
2023-05-06 22:47:57 +0200

    DatabaseException function added

--- src/main/java/at/ac/fhcampuswien/fhmdb/exception/DatabaseException.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/exception/DatabaseException.java
@@ -1,4 +1,8 @@
 package at.ac.fhcampuswien.fhmdb.exception;
 
 public class DatabaseException extends Exception {
+
+    public DatabaseException(String errorMessage){
+        super(errorMessage);
+    }
 }
