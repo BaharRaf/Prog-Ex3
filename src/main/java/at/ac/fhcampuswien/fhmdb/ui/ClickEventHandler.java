eb9d621a5aade4a10c9fd0ead87b12397bbb40ba
Sepidehri <sepidriahi@gmail.com>
2023-05-07 22:27:30 +0200

    ClickEventHandler interface added

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/ClickEventHandler.java
@@ -0,0 +1,11 @@
+package at.ac.fhcampuswien.fhmdb.ui;
+
+
+import javafx.scene.control.Button;
+
+
+
+@FunctionalInterface
+public interface ClickEventHandler<T> {
+    void onClick(T t, boolean isWatchlistCell, Button watchBtn);
+}
